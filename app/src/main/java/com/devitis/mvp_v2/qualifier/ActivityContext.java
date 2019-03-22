0f19c2669d4cbb4aa28038438d4ab0000c208be6
Diana <diana.sinenchenko@gmail.com>
2019-03-22 15:38:19 +0200

    add @interface (scope, qualifier)

---
+++ app/src/main/java/com/devitis/mvp_v2/qualifier/ActivityContext.java
@@ -0,0 +1,11 @@
+package com.devitis.mvp_v2.qualifier;
+
+import javax.inject.Qualifier;
+
+/**
+ * Created by Diana on 22.03.2019.
+ */
+
+@Qualifier
+public @interface ActivityContext {
+}
---
+++ app/src/main/java/com/devitis/mvp_v2/scope/ActivityScope.java
@@ -0,0 +1,14 @@
+package com.devitis.mvp_v2.scope;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+/**
+ * Created by Diana on 22.03.2019.
+ */
+@Scope
+@Retention(RetentionPolicy.CLASS)
+public @interface ActivityScope {
+}
